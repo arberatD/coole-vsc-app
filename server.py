819ac2a0fc6cf59bb3d994f1df4bc5c82c9f56a5
Muaz-Ar <m.m.arslan93@gmail.com>
2024-02-06 12:50:09 +0100

    feat:Endpoint GET-request Hello

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/muhammed', methods=['GET'])
+def muhammed():
+    return "Hallo, Muhammed!", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
