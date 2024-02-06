d778a63fdf4f060b4dd45ec7417032a35e304777
Muaz-Ar <m.m.arslan93@gmail.com>
2024-02-06 15:27:34 +0100

    feat:Endpoint GET-request Hello

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+<<<<<<< HEAD
 @app.route('/enrico')
 def enrico():
   return jsonify({"name": "Enrico"})
@@ -12,6 +13,11 @@ def force():
 @app.route('/flamur')
 def flamur():
     return 'Flamurs Endpoint'
+=======
+@app.route('/muhammed', methods=['GET'])
+def muhammed():
+    return "Hallo, Muhammed!", 200
+>>>>>>> 819ac2a (feat:Endpoint GET-request Hello)
 
 if __name__ == '__main__':
     app.run(debug=True)
