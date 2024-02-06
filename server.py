380b59854d9553b496489152d3bf90212af6203b
Flamur64 <flamurim64@gmail.com>
2024-02-06 13:00:15 +0100

    feat: added endpoint

--- server.py
+++ server.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/muhammed', methods=['GET'])
-def muhammed():
-    return "Hallo, Muhammed!", 200
+@app.route('/enrico')
+def enrico():
+  return jsonify({"name": "Enrico"})
+@app.route('/push-force-hard')
+def force():
+    return "Force push hard"
+
+@app.route('/flamur')
+def flamur():
+    return 'Flamurs Endpoint'
 
 if __name__ == '__main__':
     app.run(debug=True)
